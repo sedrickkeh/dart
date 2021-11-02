3659e6ea01c483af7d42ce237112e91467c36c84
Sedrick Keh <32975049+sedrickkeh@users.noreply.github.com>
2021-11-01 20:02:45 -0400

    i hope this works

--- evaluation/run_eval_on_webnlg.sh
+++ evaluation/run_eval_on_webnlg.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
+cd ../..
 
-OUTPUT_FILE=content/dart/evaluation/example/bart_webnlg.txt
+OUTPUT_FILE=dart/evaluation/example/bart_webnlg.txt
 export TEAMR=team
 
 echo $OUTPUT_FILE
 echo $TEAMR
 
-cp $OUTPUT_FILE content/dart/evaluation/webnlg-automatic-evaluation/submissions/$TEAMR.txt
+cp $OUTPUT_FILE dart/evaluation/webnlg-automatic-evaluation/submissions/$TEAMR.txt
 
 # BLEU
-cd content/dart/evaluation/webnlg-automatic-evaluation/
+cd dart/evaluation/webnlg-automatic-evaluation/
 python evaluation.py $TEAMR
 . bleu_eval_3ref.sh
 cd ..
@@ -20,7 +21,7 @@ echo "SEEN:"; cat webnlg-automatic-evaluation/eval/bleu3ref-$TEAMR\_old-cat.txt 
 echo "UNSEEN:"; cat webnlg-automatic-evaluation/eval/bleu3ref-$TEAMR\_new-cat.txt > bleu_unseen.txt
 
 # METEOR
-cd content/dart/evaluation/meteor-1.5/ 
+cd meteor-1.5/ 
 ../webnlg-automatic-evaluation/meteor_eval.sh 
 
 cd ..
@@ -31,7 +32,7 @@ echo "SEEN:"; cat webnlg-automatic-evaluation/eval/meteor-$TEAMR-old-cat.txt > m
 echo "UNSEEN:"; cat webnlg-automatic-evaluation/eval/meteor-$TEAMR-new-cat.txt > meteor_unseen.txt
 
 # TER
-cd content/dart/evaluation/meteor-1.5/tercom-0.7.25/
+cd tercom-0.7.25/
 ../webnlg-automatic-evaluation/ter_eval.sh 
 cd ..
 echo "ALL:"; cat webnlg-automatic-evaluation/eval/ter3ref-$TEAMR-all-cat.txt > ter_all.txt
@@ -41,4 +42,3 @@ echo "SEEN:"; cat webnlg-automatic-evaluation/eval/ter3ref-$TEAMR-old-cat.txt > 
 echo "UNSEEN:"; cat webnlg-automatic-evaluation/eval/ter3ref-$TEAMR-new-cat.txt > ter_unseen.txt
 
 python print_scores_webnlg.py
-
