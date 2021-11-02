85b4a376a711cbae7145142e76f4258a6639fd0c
Sedrick Keh <32975049+sedrickkeh@users.noreply.github.com>
2021-11-01 23:37:53 -0400

    edit output file path

--- evaluation/run_eval_on_webnlg.sh
+++ evaluation/run_eval_on_webnlg.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
-cd ../..
-
-OUTPUT_FILE=dart/evaluation/example/bart_webnlg.txt
+OUTPUT_FILE=webnlg_models/webnlgprefixtune_y_5_act_cat_b=5-e=5_d=0.0_u=no_lr=5e-05_w=0.0_s=101_r=n_m=512_o=1_o=1_test_beam
 export TEAMR=team
 
 echo $OUTPUT_FILE
 echo $TEAMR
 
-cp $OUTPUT_FILE dart/evaluation/webnlg-automatic-evaluation/submissions/$TEAMR.txt
+cp $OUTPUT_FILE ../../dart/evaluation/webnlg-automatic-evaluation/submissions/$TEAMR.txt
+cd ../..
 
 # BLEU
 cd dart/evaluation/webnlg-automatic-evaluation/
