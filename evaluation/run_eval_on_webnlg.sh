f7ca6cae92b2a83ad8d4eebd213138e0dc92a96d
Sedrick Keh <32975049+sedrickkeh@users.noreply.github.com>
2021-11-01 23:52:02 -0400

    Update run_eval_on_webnlg.sh

--- evaluation/run_eval_on_webnlg.sh
+++ evaluation/run_eval_on_webnlg.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-OUTPUT_FILE=webnlg_models/webnlgprefixtune_y_5_act_cat_b=5-e=5_d=0.0_u=no_lr=5e-05_w=0.0_s=101_r=n_m=512_o=1_o=1_test_beam
+OUTPUT_FILE=webnlg_models/webnlgprefixtune_y_5_act_cat_b=5-e=5_d=0.0_u=no_lr=5e-05_w=0.0_s=222_r=n_m=512_earlystop_o=1_o=1_test_beam
 export TEAMR=team
 
 echo $OUTPUT_FILE
